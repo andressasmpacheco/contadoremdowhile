cd401ab19222cbfd4512033f943078b63b0ae8e3
Andressa Simões <73480001+andressasmpacheco@users.noreply.github.com>
2023-06-09 20:52:16 -0300

    Create dez.c

---
+++ dez.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main() 
+{    
+     int numero=0;     
+     do {         
+          numero = numero +1; 
+           printf(" %d\n", numero); 
+         } 
+    while (numero <10);   
+          return 0;
+          
+           } 
